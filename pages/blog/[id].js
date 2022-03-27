6991a20a16b388fd9f1d658f491fb25f70eff4f0
geyageya <kageya@kaigai21.com>
2022-03-28 08:29:21 +0900

    third commit

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -34,3 +34,19 @@ export const getStaticProps = async (context) => {
     },
   };
 };
+
+export default function BlogId({ blog }) {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>{blog.title}</h1>
+      <p className={styles.publishedAt}>{blog.publishedAt}</p>
+      <p className="category">{blog.category && `${blog.category.name}`}</p>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `${blog.body}`,
+        }}
+        className={styles.post}
+      />
+    </main>
+  );
+}
