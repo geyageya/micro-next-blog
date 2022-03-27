b529d79d68f604b837a9dcef3c1896981643d857
geyageya <kageya@kaigai21.com>
2022-03-28 08:38:50 +0900

    5th commit

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -3,15 +3,15 @@ import { client } from "../../libs/client";
 
 export default function BlogId({ blog }) {
   return (
-    <main className={styles.main}>
-      <h1 className={styles.title}>{blog.title}</h1>
-      <p className={styles.publishedAt}>{blog.publishedAt}</p>
+    <main >
+      <h1 >{blog.title}</h1>
+      <p >{blog.publishedAt}</p>
       <p className="category">{blog.category && `${blog.category.name}`}</p>
       <div
         dangerouslySetInnerHTML={{
           __html: `${blog.body}`,
         }}
-        className={styles.post}
+        
       />
     </main>
   );
